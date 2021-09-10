2c11124ae56e5de38764904a28e33f3d7a461e8e
lawrence17920 <lawrence17920.cs07@nctu.edu.tw>
2021-09-10 19:05:36 +0800

    0910 新增初步 dataPreparation.py

--- dataPreparation.py
+++ dataPreparation.py
@@ -8,20 +8,19 @@ size, fov, heading, pitch, key = "400x400", "50", "180", "0", "API-KEY" # 設定
 
 username = "USER-NAME" # 設定"依經緯度傳換為國家"網站上的帳號名稱 (需註冊)
 
-imgCount = 1 # 設定需要找到多少張合法經緯度位置的圖片
+imgCount = 2 # 設定需要找到多少張合法經緯度位置的圖片
 
 def downloadImg():
     lat, lng = (random.random() * 90) - 90,  (random.random() * 180) - 180 # 隨機數產生經緯度
     location = str(lat) + ',' + str(lng)
     viewUrl = "https://maps.googleapis.com/maps/api/streetview?size=" + size + "&location=" + location + "&fov=" + fov + "&heading="+ heading + "&pitch=" + pitch + "&key=" + key + "&return_error_code=true"
     viewRresponse = requests.get(viewUrl)
-    print(f'Location: {location},\tStatus: {viewRresponse.status_code}')
     if viewRresponse.status_code == 200: # 回傳值為200代表合法經緯度，若為404則為非法
         img = Image.open(BytesIO(viewRresponse.content)) # 轉成 img 供以後做處理 (直接匯入 model 或先存在本機上)
         countryUrl = "http://api.geonames.org/countryCodeJSON?lat=" + str(round(lat, 2)) + "&lng=" + str(round(lng, 2)) + "&username=" + username
         countryResponse = requests.get(countryUrl)
-        countryDetail = json.loads(countryResponse.text) # 將回傳的 json 格式檔毒讀入
-        print(countryDetail["countryName"]) # 印出當中的國家名稱 (供未來作為 ground truth)
+        countryDetail = json.loads(countryResponse.text) # 將回傳的 json 格式檔讀入
+        print(f'Location: {location}\tCountry: {countryDetail["countryName"]}') # 印出其經緯度及對應的國家名稱 (供未來作為 ground truth)
         return True
     return False
 
