2ad514584afa97f0adc78ac59fdb98dc44a54cde
lawrence17920 <lawrence17920.cs07@nctu.edu.tw>
2021-09-25 18:46:31 +0800

    0925 調正成取得單一國家圖片

--- dataPreparation.py
+++ dataPreparation.py
@@ -9,34 +9,45 @@ imageKey, checkKey = "YOUR-API-KEY", "YOUR-API-KEY" # 將 Key 分兩支以確認
 
 username = "USER-NAME" # 設定"依經緯度傳換為國家"網站上的帳號名稱 (需註冊)
 
-imgCount = 3 # 設定需要找到多少張合法經緯度位置的圖片
+imgCount = 5 # 設定需要找到多少張合法經緯度位置的圖片
 
 currentCount = 0 # 目前已找到多少張圖片
 
+targetCountry = "YOUR-TARGET-COUNTRY" # 設定要尋找的目標國家
+
 def downloadImg():
-    lat, lng = (random.random() * 180) - 90,  (random.random() * 360) - 180 # 隨機數產生經緯度
+    lat, lng = (random.random() * 34) - 52,  (random.random() * 10) - 76 # 隨機數產生經緯度(在目標國家範圍下)
     location = str(lat) + ',' + str(lng)
     checkUrl = "https://maps.googleapis.com/maps/api/streetview/metadata?location=" + location + "&key=" + checkKey
     viewUrl = "https://maps.googleapis.com/maps/api/streetview?size=" + size + "&location=" + location + "&fov=" + fov + "&heading="+ heading + "&pitch=" + pitch + "&key=" + imageKey + "&return_error_code=true"
     checkRresponse = requests.get(checkUrl)
     checkDetail = json.loads(checkRresponse.text)
     if checkDetail["status"] == "OK": # 回傳值為OK代表合法經緯度，若為REQUEST_DENIED or ZERO_RESULTS則為非法
-        viewRresponse = requests.get(viewUrl)
-        img = Image.open(BytesIO(viewRresponse.content)) # 轉成 img 供以後做處理 (直接匯入 model 或先存在本機上)
         countryUrl = "http://api.geonames.org/countryCodeJSON?lat=" + str(round(lat, 2)) + "&lng=" + str(round(lng, 2)) + "&username=" + username
         countryResponse = requests.get(countryUrl)
         countryDetail = json.loads(countryResponse.text) # 將回傳的 json 格式檔讀入
-        if not os.path.exists('generated/images/'+countryDetail["countryName"]+'/'): # 檢查資料夾是否存在
-            os.mkdir('generated/images/'+countryDetail["countryName"]+'/')
-        img.save("./generated/images/" + countryDetail["countryName"] + '/image-' + str(currentCount) + ".jpg") # 將圖片儲存在路徑 'generated/images/{country}/'
-        print(f'Location: {tuple((lat, lng))}\tCountry: {countryDetail["countryName"]}') # 印出其經緯度及對應的國家名稱 (供未來作為 ground truth)
-        return True
-    return False
+        if(countryDetail["countryName"] == targetCountry): # 若此合法經緯度位於我們設定的目標國家
+            viewRresponse = requests.get(viewUrl)
+            img = Image.open(BytesIO(viewRresponse.content)) # 轉成 img 供以後做處理
+            img.save("./generated/images/" + countryDetail["countryName"] + '/image-' + str(currentCount+54) + ".jpg") # 將圖片儲存在路徑 'generated/images/{country}/'
+            print(f'Location: {tuple((lat, lng))}\tCountry: {countryDetail["countryName"]}') # 印出其經緯度及對應的國家名稱 (供未來作為 ground truth)
+            writeDetails("Location: " + str(tuple((lat, lng))) + "\tCountry: " + targetCountry + '\n') # 將地理位置存入 .txt
+            return True # 此部分情況是合法的經緯度且位於我們的目標國家
+        return False # 此部分情況是合法的經緯度但並非我們的目標國家
+    return False # 此部分情況是非法的經緯度
+
+def writeDetails(string):
+    path = 'generated/' + targetCountry + '.txt'
+    with open(path, 'a') as file:
+        file.write(string)
 
 if __name__ == '__main__':
     if not os.path.exists('generated/'):
         os.mkdir('generated/') # 產生 generated 及 images 資料夾
         os.mkdir('generated/images/')
+    
+    if not os.path.exists('generated/images/'+targetCountry+'/'): # 檢查資料夾是否存在
+        os.mkdir('generated/images/'+targetCountry+'/')
 
     start = time.time()
     while True:
