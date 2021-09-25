378eb184858e0d9154ba7ef3267cc3d1d0467aa3
lawrence17920 <lawrence17920.cs07@nctu.edu.tw>
2021-09-25 22:17:39 +0800

    0925 將資料夾分為不同用途

--- dataPreparation.py
+++ dataPreparation.py
@@ -9,14 +9,16 @@ imageKey, checkKey = "YOUR-API-KEY", "YOUR-API-KEY" # 將 Key 分兩支以確認
 
 username = "USER-NAME" # 設定"依經緯度傳換為國家"網站上的帳號名稱 (需註冊)
 
-imgCount = 5 # 設定需要找到多少張合法經緯度位置的圖片
+imgCount = 20 # 設定需要找到多少張合法經緯度位置的圖片 (一次到位，關係到檔案名稱)
 
 currentCount = 0 # 目前已找到多少張圖片
 
-targetCountry = "YOUR-TARGET-COUNTRY" # 設定要尋找的目標國家
+targetCountry = "CURRENT-TARGET-COUNTRY" # 設定要尋找的目標國家
 
-def downloadImg():
-    lat, lng = (random.random() * 34) - 52,  (random.random() * 10) - 76 # 隨機數產生經緯度(在目標國家範圍下)
+usage = 'train' # 可選擇 train 或是 val 代表目前圖片存於哪個資料夾中
+
+def downloadImg(usage='train'):
+    lat, lng = (random.random() * 24) - 42,  (random.random() * 6) - 74 # 隨機數產生經緯度(在目標國家範圍下)
     location = str(lat) + ',' + str(lng)
     checkUrl = "https://maps.googleapis.com/maps/api/streetview/metadata?location=" + location + "&key=" + checkKey
     viewUrl = "https://maps.googleapis.com/maps/api/streetview?size=" + size + "&location=" + location + "&fov=" + fov + "&heading="+ heading + "&pitch=" + pitch + "&key=" + imageKey + "&return_error_code=true"
@@ -29,15 +31,15 @@ def downloadImg():
         if(countryDetail["countryName"] == targetCountry): # 若此合法經緯度位於我們設定的目標國家
             viewRresponse = requests.get(viewUrl)
             img = Image.open(BytesIO(viewRresponse.content)) # 轉成 img 供以後做處理
-            img.save("./generated/images/" + countryDetail["countryName"] + '/image-' + str(currentCount) + ".jpg") # 將圖片儲存在路徑 'generated/images/{country}/'
+            img.save("./generated/images/" + usage + "/" + countryDetail["countryName"] + '/image-' + str(currentCount) + ".jpg") # 將圖片儲存在路徑 'generated/images/{country}/'
             print(f'Location: {tuple((lat, lng))}\tCountry: {countryDetail["countryName"]}') # 印出其經緯度及對應的國家名稱 (供未來作為 ground truth)
-            writeDetails("Location: " + str(tuple((lat, lng))) + "\tCountry: " + targetCountry + '\n') # 將地理位置存入 .txt
+            writeDetails("Location: " + str(tuple((lat, lng))) + "\tCountry: " + targetCountry + "\tUsage: " + usage + '\n') # 將地理位置存入 .txt
             return True # 此部分情況是合法的經緯度且位於我們的目標國家
         return False # 此部分情況是合法的經緯度但並非我們的目標國家
     return False # 此部分情況是非法的經緯度
 
 def writeDetails(string):
-    path = 'generated/' + targetCountry + '.txt'
+    path = 'generated/details/' + targetCountry + '.txt'
     with open(path, 'a') as file:
         file.write(string)
 
@@ -45,13 +47,18 @@ if __name__ == '__main__':
     if not os.path.exists('generated/'):
         os.mkdir('generated/') # 產生 generated 及 images 資料夾
         os.mkdir('generated/images/')
+        os.mkdir('generated/details/')
+        os.mkdir('generated/images/train/')
+        os.mkdir('generated/images/val/')
     
-    if not os.path.exists('generated/images/'+targetCountry+'/'): # 檢查資料夾是否存在
-        os.mkdir('generated/images/'+targetCountry+'/')
+    if not os.path.exists('generated/images/train/'+targetCountry+'/'): # 檢查train資料夾是否存在
+        os.mkdir('generated/images/train/'+targetCountry+'/')
+    if not os.path.exists('generated/images/val/'+targetCountry+'/'): # 檢查val資料夾是否存在
+        os.mkdir('generated/images/val/'+targetCountry+'/')
 
     start = time.time()
     while True:
-        if(downloadImg()): currentCount += 1 # 回傳為 True 代表找到圖片
+        if(downloadImg(usage)): currentCount += 1 # 回傳為 True 代表找到圖片
         if(currentCount == imgCount): break # 若以找到圖片數已達到一開始所要求數量則停止
     end = time.time()
 
