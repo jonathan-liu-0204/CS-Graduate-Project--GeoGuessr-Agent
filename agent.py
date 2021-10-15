cef41f4eeca04408f98f9d79515fd7e9037943b0
lawrence17920 <lawrence17920.cs07@nctu.edu.tw>
2021-10-16 01:59:48 +0800

    1016 優化 Terminal 顯示

--- agent.py
+++ agent.py
@@ -10,10 +10,16 @@ device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 colorama.init()
 
 model_path, label_path = 'models/europe/5.pth', 'lables/europe.txt'
+yes = ['Y', 'y']
 
 class bcolors:
-    PERFECT = '\033[42m'
-    GREAT = '\033[96m'
+    PERFECT_BACK = '\033[42m'
+    GREAT_BACK = '\033[44m'
+    GOOD_BACK = '\033[43m'
+    BAD_BACK = '\033[100m'
+    NORMAL_BACK = '\033[47m'
+    PERFECT = '\033[92m'
+    GREAT = '\033[94m'
     GOOD = '\033[93m'
     BAD = '\033[90m'
     ENDC = '\033[0m'    
@@ -44,24 +50,40 @@ def predicting(model, image, classes_path):
         labels = [line.strip() for line in f.readlines()]
     
     percentage = torch.nn.functional.softmax(out, dim=1)[0] * 100
+    _, index = torch.max(out, 1)
     _, indices = torch.sort(out, descending=True)
     rank = 1
+    if percentage[index[0]].item() > 90:
+        print(f'{bcolors.PERFECT_BACK} PERFECT {bcolors.ENDC}', end =" ")
+    elif percentage[index[0]].item() > 80:
+        print(f'{bcolors.GREAT_BACK}  GREAT  {bcolors.ENDC}', end =" ")
+    elif percentage[index[0]].item() > 70:
+        print(f'{bcolors.GOOD_BACK}  GOOD   {bcolors.ENDC}', end =" ")
+    elif percentage[index[0]].item() < 40:
+        print(f'{bcolors.BAD_BACK}   BAD   {bcolors.ENDC}', end =" ")
+    else:
+        print(f'{bcolors.NORMAL_BACK}         {bcolors.ENDC}', end =" ")
+
     for idx in indices[0][:3]:
         if(percentage[idx].item() > 90):
-            print(f'{bcolors.PERFECT}{rank}. {labels[idx]} {round(percentage[idx].item(), 2)} {bcolors.ENDC}', end =" ")
+            print(f'{bcolors.PERFECT}{labels[idx]} ({round(percentage[idx].item(), 2)}) {bcolors.ENDC}', end =" ")
         elif(percentage[idx].item() > 80):
-            print(f'{bcolors.GREAT}{rank}. {labels[idx]} {round(percentage[idx].item(), 2)} {bcolors.ENDC}', end =" ")
+            print(f'{bcolors.GREAT}{labels[idx]} ({round(percentage[idx].item(), 2)}) {bcolors.ENDC}', end =" ")
         elif(percentage[idx].item() > 70):
-            print(f'{bcolors.GOOD}{rank}. {labels[idx]} {round(percentage[idx].item(), 2)} {bcolors.ENDC}', end =" ")
+            print(f'{bcolors.GOOD}{labels[idx]} ({round(percentage[idx].item(), 2)}) {bcolors.ENDC}', end =" ")
         elif(percentage[idx].item() < 40):
-            print(f'{bcolors.BAD}{rank}. {labels[idx]} {round(percentage[idx].item(), 2)} {bcolors.ENDC}', end =" ")
+            print(f'{bcolors.BAD}{labels[idx]} ({round(percentage[idx].item(), 2)}) {bcolors.ENDC}', end =" ")
         else:
-            print(f'{rank}. {labels[idx]} {round(percentage[idx].item(), 2)}', end =" ")
+            print(f'{labels[idx]} ({round(percentage[idx].item(), 2)})', end =" ")
         rank += 1
     print()
 
 if __name__ == '__main__':
     target_model = model_loading(34, model_path)
+    img = ImageGrab.grab(bbox=SCREEN)
+    img.show()
+    if input("Check the image and press [Y/y] to start, others to quit: ") not in yes:
+        exit(0)
 
     while True:
         img = ImageGrab.grab(bbox=SCREEN)
