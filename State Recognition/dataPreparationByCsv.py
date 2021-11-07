8c957cf91b534083f68fe77645a882b27abff91c
Jonathan Liu <jonathan.tzuchi.liu@gmail.com>
2021-11-07 15:30:22 +0800

    1107 changing the directory and multiple files

--- State Recognition/dataPreparationByCsv.py
+++ State Recognition/dataPreparationByCsv.py
@@ -6,8 +6,9 @@ import csv
 
 size, fov, heading, pitch = "600x300", "90", "0", "0" # 設定 Google Street View 當中的 URL 參數 (圖片呈現格式)
 
-imageKey, checkKey = "", "" # 將 Key 分兩支以確認 checkKey 不會消耗使用量
-GettingStateKey = ""
+imageKey = ""
+checkKey = "" # 將 Key 分兩支以確認 checkKey 不會消耗使用量
+StateKey = ""
 
 username = "" # 設定"依經緯度傳換為國家"網站上的帳號名稱 (需註冊)
 
@@ -19,24 +20,32 @@ def downloadImg(imgCount, targetState, usage, latRange, latMin, lngRange, lngMin
     lat, lng = (random.random() * float(latRange)) + float(latMin),  (random.random() * float(lngRange)) + float(lngMin) # 隨機數產生經緯度(在目標國家範圍下)
     location = str(lat) + ',' + str(lng)
     checkUrl = "https://maps.googleapis.com/maps/api/streetview/metadata?location=" + location + "&key=" + checkKey
+    # print(checkUrl)
     viewUrl = "https://maps.googleapis.com/maps/api/streetview?size=" + size + "&location=" + location + "&fov=" + fov + "&heading="+ heading + "&pitch=" + pitch + "&key=" + imageKey + "&return_error_code=true"
     checkRresponse = requests.get(checkUrl)
     checkDetail = json.loads(checkRresponse.text)
     if checkDetail["status"] == "OK": # 回傳值為OK代表合法經緯度，若為REQUEST_DENIED or ZERO_RESULTS則為非法
-        countryUrl = "http://api.geonames.org/countryCodeJSON?lat=" + str(round(lat, 3)) + "&lng=" + str(round(lng, 3)) + "&username=" + username
-        countryResponse = requests.get(countryUrl)
-        countryDetail = json.loads(countryResponse.text) # 將回傳的 json 格式檔讀入
-        print(countryDetail)
-        if(countryResponse.text.find('no country code found') != -1):
-            return False # 此部分情況是合法的經緯度但並非我們的目標國家
-        # if(countryDetail["countryName"] == targetState): # 若此合法經緯度位於我們設定的目標國家
-        #     viewRresponse = requests.get(viewUrl)
-        #     img = Image.open(BytesIO(viewRresponse.content)) # 轉成 img 供以後做處理
-        #     img.save("./generated/images/" + usage + "/" + targetState + '/' + location + ".jpg") # 將圖片儲存在路徑 'generated/images/{country}/'
-        #     print(f'Number: {currentCount+1}/{imgCount}\tLocation: {tuple((lat, lng))}\tState: {targetState}') # 印出其經緯度及對應的國家名稱 (供未來作為 ground truth)
-        #     writeDetails("Location: " + str(tuple((lat, lng))) + "\tState: " + targetState + "\tUsage: " + usage + '\n', targetState) # 將地理位置存入 .txt
-        #     return True # 此部分情況是合法的經緯度且位於我們的目標國家       
-        return True # 此部分情況是合法的經緯度
+
+        StateURL = "https://maps.googleapis.com/maps/api/geocode/json?latlng=" + location + "&key=" + StateKey
+        StateChecking = requests.get(StateURL)
+        ReturnStateInfo = json.loads(StateChecking.text)
+        statecode = ReturnStateInfo["plus_code"]["compound_code"].split(",")
+
+        #print(targetState, " ---> ", statecode[1])
+        
+        if(StateChecking.text.find('no country code found') != -1):
+            return False # 此部分情況是合法的經緯度但並非我們的目標地區
+        if(statecode[1] == targetState): # 若此合法經緯度位於我們設定的目標地區
+            viewRresponse = requests.get(viewUrl)
+            img = Image.open(BytesIO(viewRresponse.content)) # 轉成 img 供以後做處理
+            img.save("./generated/images/" + usage + "/" + targetState + '/' + location + ".jpg") # 將圖片儲存在路徑 'generated/images/{state}}/'
+            print(f'Number: {currentCount+1}/{imgCount}\tLocation: {tuple((lat, lng))}\tState: {targetState}') # 印出其經緯度及對應的地區名稱 (供未來作為 ground truth)
+            writeDetails("Location: " + str(tuple((lat, lng))) + "\tState: " + targetState + "\tUsage: " + usage + '\n', targetState) # 將地理位置存入 .txt
+            return True # 此部分情況是合法的經緯度且位於我們的目標地區  
+        else:
+            print(statecode[1])
+            print("NO!! " + location + " NOT IN THE STATE!!!!")     
+        return False # 此部分情況是合法的經緯度
     return False # 此部分情況是非法的經緯度
 
 def writeDetails(string, targetState):
@@ -81,4 +90,5 @@ if __name__ == '__main__':
             if(currentCount == imgCount[i]): break # 若以找到圖片數已達到一開始所要求數量則停止
         end = time.time()
 
+        
         print(f'Download {imgCount[i]} image(s) in {round(end-start, 3)} sec(s)')
